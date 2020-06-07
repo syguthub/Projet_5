c20284bd8d01f147f73f2e256e6a7faad2799084
SouleymanGassama <syg91000@gmail.com>
2020-06-07 20:14:45 +0200

    creation class injection dans le package injections

--- todoc-master/app/src/main/java/com/cleanup/todoc/ui/ItemViewModel.java
+++ todoc-master/app/src/main/java/com/cleanup/todoc/ui/ItemViewModel.java
@@ -7,22 +7,21 @@ import com.cleanup.todoc.model.Task;
 import com.cleanup.todoc.repository.ProjectDaoRepository;
 import com.cleanup.todoc.repository.TaskDaoRepository;
 
-import java.lang.reflect.Executable;
 import java.util.List;
 import java.util.concurrent.Executor;
 
 public class ItemViewModel extends ViewModel {
 
-    private final TaskDaoRepository taskDaoRepository;
-    private final ProjectDaoRepository projectDaoRepository;
+    private final TaskDaoRepository taskDaoSource;
+    private final ProjectDaoRepository projectDaoSource;
     private final Executor executor;
 
 
     private LiveData<Task> current_Task;
 
-    public ItemViewModel(TaskDaoRepository taskDaoRepository, ProjectDaoRepository projectDaoRepository, Executor executor) {
-        this.taskDaoRepository = taskDaoRepository;
-        this.projectDaoRepository = projectDaoRepository;
+    public ItemViewModel(TaskDaoRepository taskDaoSource, ProjectDaoRepository projectDaoSource, Executor executor) {
+        this.taskDaoSource = taskDaoSource;
+        this.projectDaoSource = projectDaoSource;
         this.executor = executor;
     }
 
@@ -30,27 +29,27 @@ public class ItemViewModel extends ViewModel {
         if (this.current_Task != null){
             return;
         }
-        current_Task = this.taskDaoRepository.getTask(taskId);
+        current_Task = this.taskDaoSource.getTask(taskId);
     }
 
     public LiveData<List<Task>> getTasks(){
-        return this.taskDaoRepository.getTasks();
+        return this.taskDaoSource.getTasks();
     }
 
     public LiveData<Task> getTask(long Id){
-        return this.taskDaoRepository.getTask(Id);
+        return this.taskDaoSource.getTask(Id);
     }
 
     public void inset_Task (Task task){
-        executor.execute( ()-> this.taskDaoRepository.inset_Task(task) );
+        executor.execute( ()-> this.taskDaoSource.inset_Task(task) );
     }
 
     public void update_Task(Task task){
-        executor.execute( ()-> this.taskDaoRepository.update_Task(task) );
+        executor.execute( ()-> this.taskDaoSource.update_Task(task) );
     }
 
     public void delete_Task(long taskId){
-        executor.execute( ()-> this.taskDaoRepository.delete_Task(taskId) );
+        executor.execute( ()-> this.taskDaoSource.delete_Task(taskId) );
     }
 
 
